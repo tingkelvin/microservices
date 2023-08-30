6b50c5e953c860a52c60817fb91d49a66ccbd3e3
Kelvin Ting <ting_kelvin@icloud.com>
2023-08-30 21:08:48 +0930

    microservices

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,30 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 app.use(cors())
-const commentsByPostId = {};
+
+const commentsByPostId = {}
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id] || []);
-});
+    res.send(commentsByPostId[req.params.id] || [])
+})
 
-app.post('/posts/:id/comments', (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
-  const { content } = req.body;
+app.post('/posts/:id/comments', async (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const { content } = req.body
 
-  const comments = commentsByPostId[req.params.id] || [];
+    const comments = commentsByPostId[req.params.id] || []
 
-  comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' })
 
-  commentsByPostId[req.params.id] = comments;
+    commentsByPostId[req.params.id] = comments
 
-  res.status(201).send(comments);
-});
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending',
+        },
+    })
+
+    res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+    console.log('Recieved Event', req.body.type)
+
+    res.send({})
+})
 
 app.listen(4001, () => {
-  console.log('Listening on 4001');
-});
+    console.log('Listening on 4001')
+})
