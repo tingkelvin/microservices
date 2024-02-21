4358a887cfd356862a311365cc1adb7fd9f089b5
Kelvin Ting <ting_kelvin@icloud.com>
2024-02-21 18:02:19 +1030

    Spring init

---
+++ microservices/product-composite-service/src/test/java/kelvin/ting/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kelvin.ting.microservices.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservices/recommendation-service/src/main/java/kelvin/ting/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package kelvin.ting.microservices.core.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+
+}
