18d0539cd264a393c6bcc9dfef915bb292af9e87
Kelvin Ting <ting_kelvin@icloud.com>
2024-03-21 23:41:47 +1030

    Added data respositary

---
+++ microservices/recommendation-service/src/main/java/kelvin/ting/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,9 @@
+package kelvin.ting.microservices.core.recommendation.persistence;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+    List<RecommendationEntity> findByProductId(int productId); 
+}
